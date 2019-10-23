eb8b90590a3160fc519baf119f6ce6886d0ca40b
Ben Collerson <benc@benon.com>
2019-10-23 13:30:13 +1000

    add import chess pgn script to seeds script
    Pretty dodgy, but works well enough.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#!/usr/bin/env ruby
+# This command will automatically be run when you run "rails" with Rails 3 gems installed from the root of your application.
 
-players = 5.times.map { FactoryGirl.create(:player) }
-games = 5.times.map { FactoryGirl.create(:game) }
+GAME_DATA = '/app/data/games.pgn'
 
-games.each do |game|
-  14.times do
-    winner = players[rand(5)]
-    loser = players.reject { |p| p == winner }[rand(4)]
+def get_chess()
+  game = Game.find_by(name:"Chess")
+  if (game.nil?)
+    game = Game.create(name:"Chess", rating_type: "elo", allow_ties: true, max_number_of_teams: 2, max_number_of_players_per_team: 1, min_number_of_teams: 2, min_number_of_players_per_team: 1 )
+  end
+  return game
+end
 
-    # ResultService.create(game, winner_id: winner.id, loser_id: loser.id)
-    ResultService.create(game, teams: { "0" => { players: [winner.id]} , "1" => {players: [loser.id]} })
+def get_or_find_player(name)
+  player = Player.find_by(name: name)
+  if (player.nil?)
+    email = name.gsub(/[^a-z]/i, '').downcase + '@benon.com'
+    player = Player.new(name: name, email: email)
+    player.save
+  end
+  return player
+end
+
+def add_result(white, black, result)
+  game = get_chess()
+  if result == '1-0'
+    ResultService.create(game, teams: { "0" => {players: [white.id]}, "1" => {players: [black.id]} })
+  elsif result == '0-1'
+    ResultService.create(game, teams: { "0" => {players: [black.id]}, "1" => {players: [white.id]} })
+  else
+    ResultService.create(game, teams: { "0" => {players: [white.id], relation: 'ties'}, "1" => {players: [black.id]} })
   end
 end
 
-RatingHistoryEvent.all.each_with_index { |result, idx| result.update(created_at: DateTime.now - idx,updated_at: DateTime.now - idx)}
+white_name = nil
+black_name = nil
+result = nil
+
+File.open(GAME_DATA).each { |line|
+
+  if line =~ /White.*"([^"]*)"/
+    white_name = $1
+  elsif line =~ /Black.*"([^"]*)"/
+    black_name = $1
+  elsif line =~ /Result.*"([^"]*)"/
+    result = $1
+
+    white_player = get_or_find_player(white_name)
+    black_player = get_or_find_player(black_name)
+
+    add_result(white_player, black_player, result)
+
+    white_player = nil
+    black_player = nil
+    white_name = nil
+    black_name = nil
+    result = nil
+  end
+}
+
+# stretch out the ratings
+RatingHistoryEvent.all.sort_by{ |k| k.id }.reverse.each_with_index { |result, idx| result.update(created_at: DateTime.now - idx,updated_at: DateTime.now - idx)}
